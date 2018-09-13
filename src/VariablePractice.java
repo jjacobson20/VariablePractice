1e5db03de824d2bd51f33fdb0dc4eb8de6192ae9
jjacobson20 <43010794+jjacobson20@users.noreply.github.com>
2018-09-13 13:31:56 -0400

    added homework

---
+++ src/VariablePractice.java
@@ -0,0 +1,22 @@
+/*
+ * jeremy
+ */
+public class VariablePractice
+{
+	public static void main(String[] args)
+	{
+		String name = "jeremy";
+		
+		int length = 5;
+		int width = 10;
+		
+		int radius = 6;
+		double pi = 3.14;
+		
+		System.out.println(name);
+		System.out.println(length * width); //rectangle area
+		System.out.println(2 * pi * radius); //circle circumference
+		System.out.println(pi * radius * radius); //circle area
+		
+	}
+}
